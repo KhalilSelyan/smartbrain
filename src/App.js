8d54f6c12edae66901e18e41468c2fa3f4ee1312
Khalil Selyan <khalilselyan2@gmail.com>
2021-05-15 21:15:18 +0300

    changes for heroku

--- src/App.js
+++ src/App.js
@@ -100,17 +100,17 @@ class App extends Component {
 
   onPictureSubmit = () => {
     this.setState({ imageUrl: this.state.input });
-    fetch("http://localhost:3000/imageurl", {
-            method: "post",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({
-              input: this.state.input,
-            }),
-          })
-      .then(response => response.json())
+    fetch("https://peaceful-brook-75721.herokuapp.com/imageurl", {
+      method: "post",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        input: this.state.input,
+      }),
+    })
+      .then((response) => response.json())
       .then((response) => {
         if (response) {
-          fetch("http://localhost:3000/image", {
+          fetch("https://peaceful-brook-75721.herokuapp.com/image", {
             method: "put",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({
